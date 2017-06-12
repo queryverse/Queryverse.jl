846ef628dc35ac37f2c646efe629b11fdcec6139
David Anthoff <anthoff@berkeley.edu>
2017-06-12 16:17:23 -0700

    Reexport packages

--- src/Dataverse.jl
+++ src/Dataverse.jl
@@ -1,5 +1,17 @@
 module Dataverse
 
-# package code goes here
+using Reexport
+
+@reexport using DataValues
+@reexport using DataValueArrays
+@reexport using IterableTables
+@reexport using Query
+@reexport using DataFrames
+@reexport using FileIO
+@reexport using ExcelFiles
+@reexport using StatFiles
+@reexport using CSVFiles
+# @reexport using FeatherFiles
+@reexport using VegaLite
 
 end # module
