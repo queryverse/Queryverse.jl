8dfea6b7e44a50c4b064c3a4c7fc2966ac9f207e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-01 01:47:06 +0000

    build based on 9688c3a

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,19 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#Introduction-1","page":"Introduction","title":"Introduction","text":"","category":"section"}]
-}
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": ""
+},
+
+]}
